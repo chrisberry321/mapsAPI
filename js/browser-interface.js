75e186b3633e441a74dd136ff9b5142c152856f3
chris berry and haley bristol <chrisberry321@yahoo.com>
2016-03-17 16:55:01 -0700

    styled the page a little

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,6 +1,3 @@
-var apiKey = require('../.env').apiKey;
-var nasaKey = require('../.env').nasaKey;
-
 $(document).ready(function(){
   $('#locateUser').click(locateUser);
 });
@@ -50,7 +47,7 @@ $(document).ready(function(){
     $.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=' + date + '&api_key=C7uaHPQUmVeGK9TU4bTOkuIMDxo8d4zoJWS8utaa').then(function(response){
       console.log(response);
       var mars = response.photos[0].img_src
-      $('.picResult').append('<img src=' + mars + '>' );
+      $('.picResult').html('<div class="photo"><img src=' + mars + '></div>' );
     });
   });
 });
